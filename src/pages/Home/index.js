79de231b998ccc73df36209f4713ad467d3bd187
maniakandanramar <manikandanramar@outlook.com>
2021-06-03 13:51:56 +0530

    initial new UI added with glass slab

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,20 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
 import { func, array } from 'prop-types';
 import styles from './styles.scss';
-// import * as youtubeActions from '../../redux/modules/youtube';
-import { title, caption } from '../../config';
-// import Helmet from 'react-helmet';
-import AboutMe from '../AboutMe';
 
-// @connect(state => ({ videos: state.youtube.videos }),
-// {
-//   getVideos: youtubeActions.getVideos
-// })
 export default class Home extends PureComponent {
   static propTypes = {
     getVideos: func,
     videos: array
   }
-  // componentWillMount() {
-  //   if (!this.props.videos.length) {
-  //     this.props.getVideos();
-  //   }
-  // }
-
-  componentDidMount() {
-    // Google Analyitcs
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-106649859-2');
-  }
 
   render() {
     return (
       <div className={styles.home} >
-        Under Construction
-        {/* <AboutMe /> */}
+        <div className={styles.topContainer} ></div>
+        <div className={styles.glassContainer} >
+          <div className={styles.glass} ></div>
+        </div>
       </div>
     );
   }
